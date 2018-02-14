362f3c7adfbb6aac9974bb295b0626b1b6a078bc
James Allingham <james.allingham@gmail.com>
2018-02-14 17:45:56 +0000

    Added some command line args that will be implemented

--- src/auto_impute.py
+++ src/auto_impute.py
@@ -4,7 +4,27 @@ def main():
     pass
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Automatically repairs CSV files with missing entries")
+    parser.add_argument("file", help="name of the file to repair")
+    parser.add_argument("-v", "--verbose", help="increase the output verbosity",
+                        action="store_true")
+    speed_group = parser.add_mutually_exclusive_group()
+    speed_group.add_argument("-f", "--fast", help="quick impute",
+                            action="store_true")
+    speed_group.add_argument("-e", "--exhaustive", help="exhaustive impute",
+                             action="store_true")
+
+    output_group = parser.add_mutually_exclusive_group()
+    output_group.add_argument("-s", "--sample", help="sample from distribution",
+                            action="store_true")
+    output_group.add_argument("-m", "--mean", help="take mean of distribution",
+                            action="store_true")
+    args = parser.parse_args()
+    
+    if args.verbose:
+        print("Verbose")
+    
+    print(args.file)
     
     main()
