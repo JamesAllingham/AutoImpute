8665d8ec19bb192f6c7f32cbc905b411a6c8ebc3
James Allingham <james.allingham@gmail.com>
2018-02-15 10:50:58 +0000

    Added more command line args for reading files:
    delimiter: set the delimiter for the file being read
    
    infer_header: whether or not to treat the first row as column headers

--- src/auto_impute.py
+++ src/auto_impute.py
@@ -9,6 +9,10 @@ if __name__ == "__main__":
     parser.add_argument("file", help="name of the file to repair")
     parser.add_argument("-v", "--verbose", help="increase the output verbosity",
                         action="store_true")
+    parser.add_argument("-d", "--delimiter", help="file delimiter (default: ',')",
+                        default=",")
+    parser.add_argument("-ih", "--infer_header", help="use the first row as column names (default: true)",
+                        default="true", type=bool)
     speed_group = parser.add_mutually_exclusive_group()
     speed_group.add_argument("-f", "--fast", help="quick impute",
                             action="store_true")
