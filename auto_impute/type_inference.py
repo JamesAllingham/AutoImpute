eb968e543a05a9de74e12c08265f0953afb67238
James Allingham <james.allingham@gmail.com>
2018-02-15 11:00:59 +0000

    Added a skeleton for the type inference stuff

---
+++ auto_impute/type_inference.py
@@ -0,0 +1,10 @@
+import numpy as np
+import re
+
+class TypeInferer(object):
+    """Abstract base class for all type inference modules.
+    """
+
+class RegexTypeInferer(TypeInferer):
+    """A regex based type inference module.
+    """
