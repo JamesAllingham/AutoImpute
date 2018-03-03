1724daba498d575ef5a089b74aa165eac7b9fa80
James Allingham <james.allingham@gmail.com>
2018-03-03 16:17:45 +0000

    Added methods for deducing the type of an array based on matching each item in the array with a regex pattern

--- auto_impute/type_inference.py
+++ auto_impute/type_inference.py
@@ -1,5 +1,6 @@
 import numpy as np
 import re
+from DataType import DataType
 
 class TypeDeducer(object):
     """Abstract base class for all type inference modules.
@@ -12,27 +13,40 @@ class RegexTypeDeducer(TypeDeducer):
     """A regex based type inference module.
     """
     def deduce(self, array):
-        
+        if __test_real_num(array):
+            return DataType.Real
+        else if __test_integer_num(array):
+            return DataType.Integer
+        else if __test_date(array):
+            return DataType.Date
+        else:
+            return DataType.Unknown
 
-    def __test_disc_num(array):
+    def __do_regex_test(array, pattern):
+        passed_test = True
+        for item in array:
+            passed_test = False if not pattern.match(item) else passed_test
+        return passed_test
+
+    def __test_integer_num(array):
         int_pattern = re.compile("[0-9]+$")
+        return __do_regex_test(array, int_pattern)
 
-    def __test_cont_num(array):
+    def __test_real_num(array):
         float_pattern = re.compile("[0-9]*[.][0-9]+$|[0-9]+[.][0-9]*$")
+        return __do_regex_test(array, float_pattern)
 
     def __test_date(array):
         date_pattern = re.compile("[0-9]{2,2}[-][0-9]{2,2}[.|/|-][0-9]{2,4}$|[0-9]{2,4}[.|/|-][0-9]{2,2}[.|/|-][0-9]{2,2}$")
+        return __do_regex_test(array, date_pattern)
 
-    # sub-tests for cont:
-    def __test_cont_interval(array):
-        pass
+    # # sub-tests for cont:
+    # def __test_cont_interval(array):
+    #     pass
 
-    def __test_real_pos(array):
-        pass
+    # def __test_real_pos(array):
+    #     pass
 
-    # sub-test for disc:
-    def __test_count(array):
-        pass
-
-
-    
+    # # sub-test for disc:
+    # def __test_count(array):
+    #     pass   
