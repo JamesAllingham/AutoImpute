913070c0ba3c5bee8f4f5dcd300db084fedc6f23
James Allingham <james.allingham@gmail.com>
2018-03-15 17:21:14 +0000

    Changed the CLI arguments to represent some more simplifications

--- auto_impute/auto_impute.py
+++ auto_impute/auto_impute.py
@@ -10,19 +10,28 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--verbose", help="increase the output verbosity",
                         action="store_true")
     parser.add_argument("-d", "--delimiter", help="file delimiter (default: ',')",
-                        default=",")
-    parser.add_argument("-ih", "--infer_header", help="use the first row as column names (default: true)",
-                        default="true", type=bool)
-    speed_group = parser.add_mutually_exclusive_group()
-    speed_group.add_argument("-f", "--fast", help="quick impute",
-                            action="store_true")
-    speed_group.add_argument("-e", "--exhaustive", help="exhaustive impute",
-                             action="store_true")
+                        type=str,default=",")
+    parser.add_argument("-hd", "--header", help="use the first row as column names",
+                        action="store_false")
+    parser.add_argument("-rs", "--rand_seed", help="random seed to use (default 42)",
+                        type=int, default=42)
+
+    model_group = parser.add_mutually_exclusive_group()
+    # speed_group.add_argument("-f", "--fast", help="quick impute",
+    #                         action="store_true")
+    # speed_group.add_argument("-e", "--exhaustive", help="exhaustive impute",
+    #                          action="store_true")
+    model_group.add_argument("-mi", "--mean_imputation", help="perform mean imputation",
+                        action="store_false")
+    model_group.add_argument("-emg", "--em_gaussian", help="impute using a multivariate Gaussian fitted with EM",
+                        action="store_false")
+    model_group.add_argument("-emgmm", "--em_gaussian_mixture", help="impute using a Gaussian mixture model fitted with EM",
+                        action="store_false")
 
     output_group = parser.add_mutually_exclusive_group()
-    output_group.add_argument("-s", "--sample", help="sample from distribution",
-                            action="store_true")
-    output_group.add_argument("-m", "--mean", help="take mean of distribution",
+    output_group.add_argument("-s", "--sample", help="number of samples to take from distribution",
+                            type=int)
+    output_group.add_argument("-mm", "--mean_mode", help="take mean or mode of continuous or discrete distributions, respectively (default option)",
                             action="store_true")
     args = parser.parse_args()
     
