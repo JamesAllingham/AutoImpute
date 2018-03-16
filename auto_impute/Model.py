196cb1bef6226ff6940edf080d68d5cd8ee9e560
James Allingham <james.allingham@gmail.com>
2018-03-16 12:39:09 +0000

    Added a base class for all of the other models

---
+++ auto_impute/Model.py
@@ -0,0 +1,21 @@
+# James Allingham
+# March 2018
+# Model.py
+# Base class for all imputation models
+
+class Model(object):
+
+    def __init__(self, data):
+        """Creates the model object and fits the model to the data.
+        """
+        pass 
+
+    def impute():
+        """Imputes the missing elements of the data.
+        """
+        pass
+
+    def log_likelihood():
+        """Calculates the log likelihood of the repaired data given the model paramers.
+        """
+        pass
