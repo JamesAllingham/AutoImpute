b8b6204f17b9b7eb2afde77223cf945ac74eff60
James Allingham <james.allingham@gmail.com>
2018-03-16 15:16:39 +0000

    All of the models now use a normalised version of the input data for numerical reasons.
    The model base class now handles this so that the derived classes don't have to worry about it.

--- auto_impute/Model.py
+++ auto_impute/Model.py
@@ -3,19 +3,34 @@
 # Model.py
 # Base class for all imputation models
 
+import numpy as np
+
 class Model(object):
 
     def __init__(self, data):
         """Creates the model object and fits the model to the data.
         """
-        pass 
+        # normalise the data for numerical stability
+        self.mean = np.nanmean(data, axis=0)
+        self.std = np.nanstd(data, axis=0)
+        self.X = (data - self.mean)/self.std
 
-    def impute():
-        """Imputes the missing elements of the data.
+        self.num_examples = data.shape[0]
+        self.num_features = data.shape[1]
+
+        self.imputed_X = None
+
+    def impute(self):
+        """Returns the imputed data
         """
-        pass
+        return self.imputed_X*self.std + self.mean
+
+    def __impute():
+        """Helper function for imputing.
+        """
+        raise NotImplementedError 
 
     def log_likelihood():
         """Calculates the log likelihood of the repaired data given the model paramers.
         """
-        pass
+        raise NotImplementedError 
