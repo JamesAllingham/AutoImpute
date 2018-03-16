ffee5543260cdaf9d181d8fc21afaaa100f14a08
James Allingham <james.allingham@gmail.com>
2018-03-16 12:23:06 +0000

    Fixed bug in not passing self to class methods

--- auto_impute/csv_reader.py
+++ auto_impute/csv_reader.py
@@ -4,32 +4,33 @@
 # CSV reader for AutoImpute
 
 import numpy as np
+import pandas as pd
 
 class CSVReader(object):
     """ Manages the data read from a CSV file.
     Can return the raw data as well as masks indicating missing values and labels for column names.
     """
 
-    def __init__(file_name, delimiter=',', infer_header=True)
-        self.__raw_data = np.genfromtxt(delimiter=delimiter)
+    def __init__(self, file_name, delimiter=',', infer_header=True):
+        self.__raw_data = np.genfromtxt(file_name, delimiter=delimiter)
         
         if infer_header:
-            self.__header = raw_data[0]
-            self.__columns = raw_data[1:].T
+            self.__header = self.__raw_data[0]
+            self.__columns = self.__raw_data[1:].T
         else:
             self.__header = None
-            self.__columns = raw_data.T        
+            self.__columns = self.__raw_data.T        
 
-    def get_raw_data():
+    def get_raw_data(self):
         return self.__raw_data
 
-    def get_column_names():
+    def get_column_names(self):
         if self.__header:
             return self.__header        
         else:
             raise ValueError('No column names were inferred.')
 
-    def get_column(col_idx):
+    def get_column(self, col_idx):
         if col_idx > self.__columns.shape[0]:
             raise ValueError('Column index greater than number of columns')
         return self.__columns[col_idx]
