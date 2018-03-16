a73649b854e5f702114b2d8b9c6a569eac6584cb
James Allingham <james.allingham@gmail.com>
2018-03-16 12:05:57 +0000

    Added  a script for deterministicaly creating test data-sets

---
+++ data/data_gen.py
@@ -0,0 +1,25 @@
+# James Allingham
+# Mar 2018
+# data_gen.py
+# This script generates the test data sets, with 10-50% (in increments of 10) missing entries:
+# 1. Boston Housing
+# 2. Iris
+
+import numpy as np
+np.random.seed(42)
+import pandas as pd
+from sklearn.datasets import load_boston, load_iris
+
+def main():
+    datasets = [("boston", load_boston), ("iris", load_iris)]
+    
+    for name, load_fun in datasets:
+        X,_ = load_fun(return_X_y=True)
+        for i in range(0, 51, 10):
+            mask = np.random.rand(*X.shape) <= i/100
+            masked_X = X.copy()
+            masked_X[mask] = np.NaN
+            np.savetxt("%s-%s.csv" % (name, i), masked_X, delimiter=",")
+
+if __name__ == "__main__":
+    main()
