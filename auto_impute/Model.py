4f4e4b727fd6b5efe7c532e5caeb19c7727d5c41
James Allingham <james.allingham@gmail.com>
2018-03-17 12:20:47 +0000

    Added checks to stop fitting if LL doesnt increase.
    Added default values for the LL epsilon and number of iterations of EM
    
    Renamed num_examples to N
    
    Split log_likelihood into two functions: __log_likelihood, which does the actual calculation and log_likelihood which just returns the value

--- auto_impute/Model.py
+++ auto_impute/Model.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class Model(object):
 
-    def __init__(self, data):
+    def __init__(self, data, ϵ=None, max_iters=None):
         """Creates the model object and fits the model to the data.
         """
         # normalise the data for numerical stability
@@ -15,22 +15,38 @@ class Model(object):
         self.std = np.nanstd(data, axis=0)
         self.X = (data - self.mean)/self.std
 
-        self.num_examples = data.shape[0]
+        self.N = data.shape[0]
         self.num_features = data.shape[1]
 
         self.imputed_X = None
+        self.ll = None
+
+        if (ϵ is None):
+            self.ϵ = 1e-1
+        else:
+            self.ϵ = ϵ
+
+        if (max_iters is None):
+            self.max_iters = 1000
+        else:
+            self.max_iters = max_iters
 
     def impute(self):
         """Returns the imputed data
         """
         return self.imputed_X*self.std + self.mean
 
-    def __impute():
+    def __impute(self):
         """Helper function for imputing.
         """
         raise NotImplementedError 
 
-    def log_likelihood():
+    def log_likelihood(self):
         """Calculates the log likelihood of the repaired data given the model paramers.
+        """
+        return self.ll 
+
+    def __log_likelihood(self):
+        """Helper function for log likelihood.
         """
         raise NotImplementedError 
--- auto_impute/SingleGaussianEM.py
+++ auto_impute/SingleGaussianEM.py
@@ -11,14 +11,17 @@ from scipy import linalg
 
 class SingleGaussian(Model.Model):
 
-    def __init__(self, data):
-        Model.Model.__init__(self, data)        
+    def __init__(self, data, ϵ=None, max_iters=None):
+        Model.Model.__init__(self, data, ϵ=ϵ, max_iters=max_iters)        
         self.μ = np.random.rand(self.num_features)
         self.Σ = np.random.rand(self.num_features, self.num_features)
         self.imputed_X = None
 
         # fit the model to the data
-        for i in range(10):
+        best_ll = -np.inf
+        old_μ, old_Σ, old_imputed_X = self.μ, self.Σ, self.imputed_X
+
+        for i in range(self.max_iters):
             if i == 0:
                 # using the current parameters, estimate the values of the missing data:
                 # impute by taking the mean of the conditional distro
@@ -27,10 +30,10 @@ class SingleGaussian(Model.Model):
             # now re-estimate μ and Σ
             self.μ = np.mean(self.imputed_X, axis = 0)
             self.Σ = np.zeros_like(self.Σ)
-            for i in range(self.num_examples):
-                diff = self.imputed_X[i,:] - self.μ
+            for j in range(self.N):
+                diff = self.imputed_X[j,:] - self.μ
                 self.Σ += np.outer(diff, diff.T)
-            self.Σ = self.Σ/self.num_examples
+            self.Σ = self.Σ/self.N
 
             # regularisation term ensuring that the cov matrix is always pos def
             self.Σ += np.diag(np.ones(shape=(self.num_features,))*1e-6)
@@ -38,11 +41,20 @@ class SingleGaussian(Model.Model):
             # using the current parameters, estimate the values of the missing data:
             # impute by taking the mean of the conditional distro
             self.__impute()
+
+            # if the log likelihood stops improving then stop iterating
+            self.__log_likelihood()
+            if (self.ll < best_ll or self.ll - best_ll < self.ϵ):
+                self.μ, self.Σ, self.imputed_X = old_μ, old_Σ, old_imputed_X
+                self.ll = best_ll
+                break
+            else:
+                best_ll = self.ll
             
     def __impute(self):
         Λ = linalg.inv(self.Σ)
         imputed_X = self.X.copy()
-        for i in range(self.num_examples):
+        for i in range(self.N):
             X_row = imputed_X[i,:]
             # if there are no missing values then go to next iter
             if np.all(~np.isnan(X_row)): continue
@@ -62,10 +74,10 @@ class SingleGaussian(Model.Model):
                 μmo -= linalg.inv(Λmm) @ Λmo @ (X_row[o_locs] - self.μ[o_locs])
 
             imputed_X[i,:][m_locs] = μmo
-            self.imputed_X = imputed_X
+        self.imputed_X = imputed_X
 
-    def log_likelihood(self):
+    def __log_likelihood(self):
         ll = 0
-        for i in range(self.num_examples):
+        for i in range(self.N):
             ll += stats.multivariate_normal.pdf(self.imputed_X[i,:], mean=self.μ, cov=self.Σ)
-        return np.log(ll)
+        self.ll = np.log(ll)
