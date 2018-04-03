Changed to using mean LL and fixed some LL calculation bugs
The LL calculation for the SG now takes the log before summing.

The LL calculation for the MI now considers the probability of seeing a whole observation at once.

auto_impute/GMM_EM.py
@@ -153,7 +153,7 @@ class GMM(Model):
             for j in range(self.num_gaussians):
                 tmp += self.ps[i,j] * stats.multivariate_normal.pdf(self.expected_X[i,:], mean=self.μs[j,:], cov=self.Σs[j,:,:])
             ll += np.log(tmp)
-        self.ll = ll  
+        self.ll = ll/self.N
 
     def sample(self, n):
         sampled_Xs = np.stack([self.X]*n, axis=0)

auto_impute/MeanImpute.py
@@ -18,7 +18,12 @@ class MeanImpute(Model):
         missing_locs = np.isnan(self.X)
         self.expected_X[missing_locs] = means[np.where(missing_locs)[1]]
 
-        self.ll = np.log(np.sum(np.stack([means]*self.N, axis=0) == self.expected_X))
+        # self.ll = np.log(np.sum(np.stack([means]*self.N, axis=0) == self.expected_X))
+        ll = 0
+        for i in range(self.N):
+            ll += np.log(np.array_equal(means, self.expected_X[i,:]))
+        self.ll = ll/self.N
+
 
     def sample(self):
         warnings.warn("Cannot sample from a mean imputation. Returning the means.")

auto_impute/SingleGaussianEM.py
@@ -83,8 +83,8 @@ class SingleGaussian(Model):
     def __log_likelihood(self):
         ll = 0
         for i in range(self.N):
-                ll += stats.multivariate_normal.pdf(self.expected_X[i,:], mean=self.μ, cov=self.Σ)
-        self.ll = np.log(ll)
+                ll += np.log(stats.multivariate_normal.pdf(self.expected_X[i,:], mean=self.μ, cov=self.Σ))
+        self.ll = ll/self.N
 
     def sample(self, n):
         sampled_Xs = np.stack([self.X.copy()]*n, axis=0)