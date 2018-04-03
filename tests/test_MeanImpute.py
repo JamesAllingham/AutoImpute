051088a77388b1e7fc0ba08a8ff042c69b5739bb
James Allingham <james.allingham@gmail.com>
2018-04-03 16:09:16 +0100

    Added test cases for when the data has a one and two observed value(s) for each feature

--- tests/test_MeanImpute.py
+++ tests/test_MeanImpute.py
@@ -17,9 +17,12 @@ class NoMissingValuesBaseTestCase(unittest.TestCase):
 
     def setUp(self):
         # create a fake data file to impute
-        self.data = np.array([[1,2,3],
-                         [4,5,6],
-                         [7,8,9]])
+        self.data = np.array(
+            [[1,2,3],
+             [4,5,6],
+             [7,8,9]], 
+             dtype=np.float32
+        )
 
 class NoMissingValuesRMSETestCase(NoMissingValuesBaseTestCase):
 
@@ -40,7 +43,63 @@ class NoMissingValuesLLTestCase(NoMissingValuesBaseTestCase):
 
         self.assertEqual(ll, -np.inf)
 
-class AllMissingValuesTest(unittest.TestCase):
+class OneValueBaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.data = np.array(
+            [[1     , np.nan, np.nan],
+             [np.nan, 2     , np.nan],
+             [np.nan, np.nan, 3     ]],
+             dtype=np.float32
+        )
+
+class OneValueLLTestCase(OneValueBaseTestCase):
+
+    def runTest(self):
+        model = MeanImpute(self.data, verbose=False)
+
+        ll = model.log_likelihood()
+
+        self.assertEqual(ll, 0)
+
+class OneValueResultTestCase(OneValueBaseTestCase):
+
+    def runTest(self):
+        model = MeanImpute(self.data, verbose=False)
+
+        result = model.impute()
+
+        self.assertTrue(np.array_equal(result, np.array([1,2,3,1,2,3,1,2,3]).reshape(3,3)))
+
+class TwoValuesBaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.data = np.array(
+            [[1     , 3     , 5     ],
+             [2     , 4     , 6     ],
+             [np.nan, np.nan, np.nan]],
+             dtype=np.float32
+        )
+
+class TwoValuesLLTestCase(TwoValuesBaseTestCase):
+
+    def runTest(self):
+        model = MeanImpute(self.data, verbose=False)
+
+        ll = model.log_likelihood()
+
+        self.assertEqual(ll, -np.inf)
+
+class TwoValuesResultTestCase(TwoValuesBaseTestCase):
+
+    def runTest(self):
+        model = MeanImpute(self.data, verbose=False)
+
+        result = model.impute()
+
+        self.assertTrue(np.array_equal(result, np.array([1,3,5,2,4,6,1.5,3.5,5.5]).reshape(3,3)))
+
+class AllMissingValuesTestCase(unittest.TestCase):
 
     def setUp(self):
         self.data = np.array([np.nan]*9).reshape(3,3)
@@ -48,3 +107,21 @@ class AllMissingValuesTest(unittest.TestCase):
     def runTest(self):
         with self.assertRaises(RuntimeError):
             model = MeanImpute(self.data, verbose=False)
+
+class NoRowsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.data = np.zeros(shape=(0,3), dtype=np.float32)
+
+    def runTest(self):
+        with self.assertRaises(RuntimeError):
+            model = MeanImpute(self.data, verbose=True)
+
+class NoColsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.data = np.zeros(shape=(3,0), dtype=np.float32)
+
+    def runTest(self):
+        with self.assertRaises(RuntimeError):
+            model = MeanImpute(self.data, verbose=True)
