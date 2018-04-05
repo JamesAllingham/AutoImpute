8e55b35b76f4308b2bd5c8582f0c7edd0dd5ff85
James Allingham <james.allingham@gmail.com>
2018-04-05 11:16:42 +0100

    Added temporary GMM test cases

---
+++ tests/test_GMM_EM.py
@@ -0,0 +1,37 @@
+# James Allingham
+# April 2018
+# test_SingleGaussianEM.py
+# Tests for the single Gaussian EM module
+
+import unittest
+import sys
+import numpy as np
+
+# add both the relative and absolute paths for the code to test
+sys.path.append("../auto_impute/")
+sys.path.append("auto_impute/")
+
+from GMM_EM import GMM
+import test_utils
+
+class BostonMCAR10LLTestCase(test_utils.BostonMCAR10BaseTestCase):
+
+    def runTest(self):
+        np.random.seed(42)
+        model = GMM(self.data, 3, verbose=False)
+        model.fit()
+
+        ll = model.log_likelihood()
+
+        self.assertAlmostEqual(ll, -33.0724832763343)
+
+class BostonMCAR30LLTestCase(test_utils.BostonMCAR30BaseTestCase):
+
+    def runTest(self):
+        np.random.seed(42)
+        model = GMM(self.data, 3, verbose=False)
+        model.fit()
+
+        ll = model.log_likelihood()
+
+        self.assertAlmostEqual(ll, -31.71329520444794)
