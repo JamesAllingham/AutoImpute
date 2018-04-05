a1a48e729c531e7cdb8dbb2995cf3236b8ba017a
James Allingham <james.allingham@gmail.com>
2018-04-05 10:55:14 +0100

    Added a bunch of temporary base test cases to help with refactoring

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -35,3 +35,83 @@ class TwoValuesBaseTestCase(unittest.TestCase):
              [np.nan, np.nan, np.nan]],
              dtype=np.float32
         )
+
+# Temporary test cases - not permanent features, just here to make sure I don't break things while refactoring code
+class BostonMCAR10BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/boston-10-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/boston-10-MCAR.csv", delimiter=",")
+
+class BostonMCAR20BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/boston-20-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/boston-20-MCAR.csv", delimiter=",")
+
+class BostonMCAR30BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/boston-30-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/boston-30-MCAR.csv", delimiter=",")
+
+class BostonMCAR40BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/boston-40-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/boston-40-MCAR.csv", delimiter=",")
+
+class BostonMCAR50BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/boston-50-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/boston-50-MCAR.csv", delimiter=",")
+
+class IrisMCAR10BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/iris-10-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/iris-10-MCAR.csv", delimiter=",")
+
+class IrisMCAR20BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/iris-20-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/iris-20-MCAR.csv", delimiter=",")
+
+class IrisMCAR30BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/iris-30-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/iris-30-MCAR.csv", delimiter=",")
+
+class IrisMCAR40BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/iris-40-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/iris-40-MCAR.csv", delimiter=",")
+
+class IrisMCAR50BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.data = np.genfromtxt("../data/iris-50-MCAR.csv", delimiter=",")
+        except FileNotFoundError as e:
+            self.data = np.genfromtxt("data/iris-50-MCAR.csv", delimiter=",")
