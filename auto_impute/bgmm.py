de2ab7d93b1479122c836fb2e797ea6da9228f30
James Allingham <james.allingham@gmail.com>
2018-04-13 15:46:47 +0100

    Based the initial guesses for the paramters on the k-means clustering.
    Added considerations for the cases where there are no observations in a row.
    
    Refactored the calculations for the responsibilties to make them clearer

--- auto_impute/bgmm.py
+++ auto_impute/bgmm.py
@@ -4,11 +4,14 @@
 # Imputation using a Gaussian Mixture Model fitted using variational Bayes
 
 from model import Model
+from utilities import regularise_Σ
 
 import numpy as np
+import numpy.ma as ma
 from scipy import stats
 from scipy import linalg
 from scipy import special
+from sklearn.cluster import KMeans
 
 class BGMM(Model):
 
@@ -23,18 +26,31 @@ class BGMM(Model):
         self.W0 = np.eye(self.num_features)
         self.ν0 = self.num_features
 
-        self.rs = None
+        self.rs = np.zeros(shape=(self.N, self.num_gaussians))
 
-        # updated params
+        # intial params
+        # use k-means to initialise the means and covs
+        self.ms  = [np.zeros(shape=(self.num_features,))]*self.num_gaussians
+        self.Ws = [np.zeros(shape=(self.num_features, self.num_features))]*self.num_gaussians
+        mean_imputed_X = self.X.data.copy()
+        mean_imputed_X[self.X.mask] = ma.mean(self.X, axis=0)[np.where(self.X.mask)[1]]
+        # mean_imputed_X += np.random.random(mean_imputed_X.shape)*0.01
+        kmeans = KMeans(n_clusters=self.num_gaussians, random_state=0).fit(mean_imputed_X)
+        self.rs[np.arange(self.N), kmeans.labels_] = 1
+        # for k in range(num_gaussians):
+        #     locs = np.where(kmeans.labels_ == k)[0]
+        #     self.ms[k] = np.mean(mean_imputed_X[locs, :], axis=0)
+        #     self.Ws[k] = np.cov(mean_imputed_X[locs, :], rowvar=False)
+        
+        # Ns = np.sum(self.rs, axis=0)
+
+        # self.αs = [Ns[k] + self.α0 for k in range(self.num_gaussians)]
+
         self.αs = [self.α0]*self.num_gaussians
-        self.ms = [self.m0]*self.num_gaussians
         self.βs = [self.β0]*self.num_gaussians
-        self.Ws = [self.W0]*self.num_gaussians
         self.νs = [self.ν0]*self.num_gaussians
 
-        self._calc_rs()
-        self._calc_ML_est()
-        self._calc_ll()
+        self._calc_updated_params()
 
     def fit(self, max_iters=100, ϵ=1e-1):
         if self.verbose: print("Fitting model:")
@@ -59,36 +75,42 @@ class BGMM(Model):
     # "E-step"
     def _calc_rs(self):
         log_πs = special.digamma(self.αs) - special.digamma(np.sum(self.αs))
-        
         log_ps = np.stack([log_πs]*self.N, axis=0)
         for n in range(self.N):
             o_locs = np.where(~self.X[n, :].mask)[0]
+            
+            if not o_locs.size: continue
+
             oo_coords = tuple(zip(*[(i, j) for i in o_locs for j in o_locs]))
             x_row = self.X[n,:].data
-            log_ps[n, :] += 0.5*np.log(np.array([linalg.det(self.Ws[k][oo_coords].reshape(len(o_locs), len(o_locs))) for k in range(self.num_gaussians)]))
+            log_ps[n, :] += 0.5*np.log(np.array([linalg.det(self.Ws[k][oo_coords].reshape(o_locs.size, o_locs.size)) for k in range(self.num_gaussians)]))
 
-            log_ps[n, :] += np.array(
-                    [0.5*np.sum(
-                            [special.digamma(0.5*(self.νs[k] - self.num_features + len(o_locs) + 1 - i)) for i in range(len(o_locs))]
-                        ) for k in range(self.num_gaussians)
-                    ]
-                )
-            log_ps[n, :] -= 0.5*np.array([self.βs[k]*(self.νs[k] - self.num_features + len(o_locs))*(x_row[o_locs] - self.ms[k][o_locs]).T @ self.Ws[k][oo_coords].reshape(len(o_locs), len(o_locs)) @ (x_row[o_locs] - self.ms[k][o_locs]) for k in range(self.num_gaussians)]) 
+            for k in range(self.num_gaussians):
+                log_ps[n, k] += 0.5*np.sum([special.digamma(0.5*(self.νs[k] - self.num_features + o_locs.size + 1 - i)) for i in range(o_locs.size)]) 
 
-        ps = np.exp(log_ps) + 1e-9
-        self.rs = ps/np.sum(ps, axis=1, keepdims=True)
-        
-        print(self.rs)
+            for k in range(self.num_gaussians):
+                tmp = 0.5*self.βs[k]
+                tmp *= (self.νs[k] - self.num_features + o_locs.size)
+                tmp *= (x_row[o_locs] - self.ms[k][o_locs]).T
+                tmp = tmp @ self.Ws[k][oo_coords].reshape(o_locs.size, o_locs.size)
+                tmp = tmp @ (x_row[o_locs] - self.ms[k][o_locs])
+                log_ps[n, k] -= tmp
+
+        ps = np.exp(log_ps) + 1e-9 # incase none of the components want to take charge of an example
+        self.rs = ps/np.sum(ps, axis=1, keepdims=True)        
 
     # "M-step"
     def _calc_updated_params(self):
         Ns = np.sum(self.rs, axis=0)
 
-        self.αs = Ns + self.α0
+        self.αs = [Ns[k] + self.α0 for k in range(self.num_gaussians)]
 
         prev_ms = self.ms.copy()
         
         for k in range(self.num_gaussians):
+            # check if the component is still valid
+            if Ns[k] == 0: continue
+
             # build a repaired version of the data to use for the subsequent calculations
             x_rep = self.X.data.copy()
             for n in range(self.N):
@@ -127,8 +149,8 @@ class BGMM(Model):
                 Xs[k, n, m_locs] = μ_k[m_locs]
 
                 if o_locs.size:
-                    Σ = np.linalg.inv(Λ_k[mm_coords].reshape(len(m_locs), len(m_locs)))
-                    Xs[k, n, m_locs] -= Σ @ Λ_k[mo_coords].reshape(len(m_locs), len(o_locs)) @ (x_row[o_locs] - μ_k[o_locs])
+                    Σ = np.linalg.inv(Λ_k[mm_coords].reshape(m_locs.size, m_locs.size))
+                    Xs[k, n, m_locs] -= Σ @ Λ_k[mo_coords].reshape(m_locs.size, o_locs.size) @ (x_row[o_locs] - μ_k[o_locs])
                 
         self.expected_X = np.zeros_like(self.X.data)
         for k in range(self.num_gaussians):
@@ -154,12 +176,11 @@ class BGMM(Model):
                 Λ_k = self.νs[k]*self.Ws[k]
 
                 # now the mean and var for the missing data given the seen data
-                var = np.linalg.inv(Λ_k[mm_coords].reshape(len(m_locs), len(m_locs)))
+                var = np.linalg.inv(Λ_k[mm_coords].reshape(m_locs.size, m_locs.size))
                 mean = μ_k[m_locs]
                 if o_locs.size:
-                    mean -= var @ Λ_k[mo_coords].reshape(len(m_locs), len(o_locs)) @ (x_row[o_locs] - μ_k[o_locs])
+                    mean -= var @ Λ_k[mo_coords].reshape(m_locs.size, o_locs.size) @ (x_row[o_locs] - μ_k[o_locs])
 
-                
                 tmp += self.rs[n, k] * stats.multivariate_normal.pdf(self.expected_X[n, m_locs], mean=mean, cov=var)
 
             ll += np.log(tmp)
