c3d4b2f37d6877486e1640137ef7be523fa94e1e
James Allingham <james.allingham@gmail.com>
2018-04-29 19:26:24 +0100

    Implemented M-step, ML_est, LL and sample methods
    Still buggy

--- auto_impute/mmm.py
+++ auto_impute/mmm.py
@@ -54,10 +54,10 @@ class MMM(Model):
         self.rs[np.arange(self.N), kmeans.labels_] = 1
 
         # create parameters depending on the feature assingments
-        self.μs = np.stack([np.mean(mean_imputed_X[np.where(kmeans.labels_ == k)[0], self.real_columns], axis=0) for k in range(self.num_components)], axis=0)
-        self.Σs = np.stack([regularise_Σ(np.diag(np.var(mean_imputed_X[np.where(kmeans.labels_ == k)[0], self.real_columns], axis=0))) for k in range(self.num_components)], axis=0)
+        self.μs = np.stack([np.mean(mean_imputed_X[np.where(kmeans.labels_ == k)[0], :], axis=0) for k in range(self.num_components)], axis=0)
+        self.Σs = np.stack([regularise_Σ(np.diag(np.var(mean_imputed_X[np.where(kmeans.labels_ == k)[0], :], axis=0))) for k in range(self.num_components)], axis=0)
         self.ps = np.array([[np.mean([self.one_hot_lookups[d][self.X.data[n, d]] for n in self.N if self.X.mask[n, d]], axis=0) 
-            for d, is_real in enumerate(self.real_columns) if not is_real] for k in range(self.num_components)])
+            for d in range(self.num_features)] for k in range(self.num_components)])
 
         # randomise initial responsibilities
         self.rs = np.random.dirichlet(np.ones((self.N,)), self.num_components).T
@@ -97,10 +97,8 @@ class MMM(Model):
             mask_row = self.X[n, :].mask
             
             if not np.all(mask_row):
-                r_locs = np.where(self.real_columns)[0]
-                d_locs = ~np.where(self.real_columns)[0]
-                o_r_locs = np.where(~mask_row[r_locs])[0] 
-                o_d_locs = np.where(~mask_row[d_locs])[0] 
+                o_r_locs = np.where(np.logical_and(~mask_row, self.real_columns))[0] 
+                o_d_locs = np.where(np.logical_and(~mask_row, ~self.real_columns))[0] 
                 
                 rs[n, :] = np.ones((self.num_components,))
 
@@ -112,21 +110,121 @@ class MMM(Model):
                         for k in range(self.num_components)])
                 # if there are any observed discrete vars then include them
                 if len(o_d_locs):
-                    rs[n, :] *= np.array([np.prod([stats.multinomial.pmf(self.one_hot_lookups[d][x_row[d_locs][d]], 1, self.ps[d][:, k]) for d in o_d_locs])
+                    rs[n, :] *= np.array([np.prod([stats.multinomial.pmf(self.one_hot_lookups[d][x_row[o_d_locs][d]], 1, self.ps[d][:, k]) for d in o_d_locs])
                         for k in range(self.num_components)])
             else:
                 rs[n, :] = np.mean(self.rs, axis=0)
 
     # M-step
     def _update_params(self):
-        
+        # update the ps
+        ps = np.array([np.zeros(shape=(self.unique_vals[d].size, self.num_components)) for d in range(self.num_features)])
+
+        for d, is_real in enumerate(self.real_columns):
+            if is_real: continue
+            
+            for k in range(self.num_components):
+                
+                tmp = 0
+                for n in range(self.N):
+                    tmp += self.rs[n, k]*(self.one_hot_lookups[d][self.X.data[n, d]] if not self.X.mask[n, d] else self.ps[d][:, k])
+
+                tmp /= np.sum(self.rs[:, k])
+                ps[d][:, k] = tmp
+
+        self.ps = ps
+
+        for k in range(self.num_components):
+            # update the μs
+            # first fill in the missing elements of X
+            X = self.X.data
+            μ = np.stack([self.μs[k]]*self.N, axis=0)
+            X[self.X.mask] = μ[self.X.mask] # TODO: make this work when dealing with full cov matrices
+
+            p = self.rs[:, k]
+            self.μs[k] = (p @ X)/np.sum(p)
+
+            # now do Σs
+            C = np.zeros(shape=(self.num_features, self.num_features))
+            for n in range(self.N):
+                mask_row = self.X[n, :].mask
+                C[np.ix_(mask_row, mask_row)] += self.Σs[k][np.ix_(mask_row, mask_row)] # TODO: make this work with full cov matrices
+            diff = (X - self.μs[k])*np.sqrt(p)
+            self.Σs[k] = np.diag(np.sum(diff*diff, axis=0)/np.sum(p)) # Note this will need to change to an outer product for full cov something like np.einsum('ij,ik->ijk', foo, foo)
+
+            self.Σs[k] += C
+            self.Σs[k] = regularise_Σ(self.Σs[k])
 
     def _calc_ML_est(self):
-        pass
+        for n in range(self.N):
+            x_row = self.X[n, :].data
+            mask_row = self.X[n, :].mask
+
+            if np.all(~mask_row): continue
+            
+            m_r_locs = np.where(np.logical_and(mask_row, self.real_columns))[0] 
+            m_d_locs = np.where(np.logical_and(mask_row, ~self.real_columns))[0] 
+
+            # impute real values
+            if m_r_locs.size:
+                self.expected_X[n, :][m_r_locs] = np.sum(self.rs[n, :]*self.μs[m_r_locs], axis=0) # TODO: make this work with full cov
+
+            # impute discrete values
+            for d in m_d_locs:
+                self.expected_X[n, d] = self.unique_vals[d][np.argmax(np.sum(self.rs[n, :]*self.ps[d][:, :], axis=1))]
 
     def _calc_ll(self):
-        pass
+        lls = []
+
+        for n in range(self.N):
+            mask_row = self.X[n, :].mask
+            if np.all(~mask_row): continue
+
+            m_r_locs = np.where(np.logical_and(mask_row, self.real_columns))[0] 
+            m_d_locs = np.where(np.logical_and(mask_row, ~self.real_columns))[0] 
+
+            prob = 0
+            for k in range(self.num_components):
+                tmp = 1
+
+                # probability of real values
+                μmo = self.μs[k, m_r_locs] # TODO: make this work with full cov mat
+                Σmm = self.Σs[k, :, :][np.ix_(m_r_locs, m_r_locs)]
+
+                tmp *= self.rs[n, k] * stats.multivariate_normal.pdf(self.expected_X[n, m_r_locs], mean=μmo, cov=Σmm, allow_singular=True)
+
+                # probability of discrete values
+                for d in m_d_locs:
+                    tmp *= self.rs[n, k]*stats.multinomial.pmf(self.one_hot_lookups[d][self.expected_X[n, d]] ,1, self.ps[d][:, k])
+
+                prob += tmp
+
+            lls.append(np.log(prob)) 
+
+        self.ll = np.mean(lls)
 
     def sample(self, num_samples):
-        pass
+        sampled_Xs = np.stack([self.X.data]*num_samples, axis=0)
+
+        for i in range(num_samples):
+
+            for n in range(self.N):
+                mask_row = self.X[n, :].mask
+                # if there are no missing values then go to next iter
+                if np.all(~mask_row): continue
+
+                m_r_locs = np.where(np.logical_and(mask_row, self.real_columns))[0] 
+                m_d_locs = np.where(np.logical_and(mask_row, ~self.real_columns))[0] 
+                k = np.random.choice(self.num_components, p=self.rs[n, :])
+                
+                # sample real values
+                μmo = self.μs[k, m_r_locs] # TODO: make this work with full cov mat
+                Σmm = self.Σs[k, :, :][np.ix_(m_r_locs, m_r_locs)]
+                sampled_Xs[i, n, m_r_locs] = stats.multivariate_normal.rvs(mean=μmo, cov=Σmm, size=1)
+
+                # sample discrete values
+                for d in m_d_locs:
+                    sampled_Xs[i, n, d] = self.unique_vals[d][np.argmax(stats.multinomial.rvs(1, self.ps[k, d]))]
+
+        return sampled_Xs
         
