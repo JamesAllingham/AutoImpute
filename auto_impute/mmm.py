5230e7faab850440530b02a3b22015ba132d426a
James Allingham <james.allingham@gmail.com>
2018-04-30 21:36:50 +0100

    Mixed mixture model now seemingly works!
    The fix was to now use random initialisation for the categorical parameters

--- auto_impute/mmm.py
+++ auto_impute/mmm.py
@@ -13,7 +13,6 @@ from utilities import encode_1_hot, regularise_Σ
 import numpy as np
 import numpy.ma as ma
 from scipy import stats
-from scipy import linalg
 from sklearn.cluster import KMeans
 
 class MMM(Model):
@@ -41,23 +40,24 @@ class MMM(Model):
             if assignment != 'r' and assignment != 'd':
                 raise RuntimeError("Invalid assignment ('%s') given for column %s. Use 'r' and 'd' for real and discrete valued columns respectively." % (assignment, d))
         
-        self.real_columns = np.array([assignment != 'r' for assignment in assignments])
+        self.real_columns = np.array([assignment == 'r' for assignment in assignments])
 
-        # use k-means to initialise params
-        self.rs = np.zeros(shape=(self.N, self.num_components))
+        # use k-means to initialise gaussian params
         mean_imputed_X = self.X.data.copy()
         mean_imputed_X[self.X.mask] = ma.mean(self.X, axis=0)[np.where(self.X.mask)[1]]
         kmeans = KMeans(n_clusters=self.num_components, random_state=0).fit(mean_imputed_X)
-        self.rs[np.arange(self.N), kmeans.labels_] = 1
 
         # create parameters depending on the feature assingments
         self.μs = np.stack([np.mean(mean_imputed_X[np.where(kmeans.labels_ == k)[0], :], axis=0) for k in range(self.num_components)], axis=0)
         self.Σs = np.stack([regularise_Σ(np.diag(np.var(mean_imputed_X[np.where(kmeans.labels_ == k)[0], :], axis=0))) for k in range(self.num_components)], axis=0)
-        self.ps = np.array([[np.mean([self.one_hot_lookups[d][self.X.data[n, d]] for n in range(self.N) if ~self.X.mask[n, d]], axis=0) 
+        # self.ps = np.array([[np.mean([self.one_hot_lookups[d][self.X.data[n, d]] for n in range(self.N) if ~self.X.mask[n, d]], axis=0) 
+        #     for d in range(self.num_features)] for k in range(self.num_components)])
+        # randomly init the catagorical params
+        self.ps = np.array([[np.random.dirichlet(np.ones(len(self.unique_vals[d]))) 
             for d in range(self.num_features)] for k in range(self.num_components)])
 
         # randomise initial responsibilities
-        self.rs = np.random.dirichlet(np.ones((self.N,)), self.num_components).T
+        self.rs = np.random.dirichlet(np.ones((self.N,))*10, self.num_components).T
 
         self._calc_ML_est()
         self._calc_ll()
@@ -67,7 +67,6 @@ class MMM(Model):
         if self.verbose: print("Fitting model:")
         for i in range(max_iters):
             old_μs, old_Σs, old_ps, old_rs, old_expected_X = self.μs.copy(), self.Σs.copy(), self.ps.copy(), self.rs.copy(), self.expected_X.copy()
-
             # E-step
             self._calc_rs()
 
@@ -84,6 +83,7 @@ class MMM(Model):
             
             best_ll = self.ll
             if self.verbose: print("Iter: %s\t\tLL: %f" % (i, self.ll))
+        print("")
 
     # E-step
     def _calc_rs(self):
@@ -95,7 +95,7 @@ class MMM(Model):
             if not np.all(mask_row):
                 o_r_locs = np.where(np.logical_and(~mask_row, self.real_columns))[0] 
                 o_d_locs = np.where(np.logical_and(~mask_row, np.logical_not(self.real_columns)))[0] 
-                
+             
                 rs[n, :] = np.ones((self.num_components,))
 
                 # if there are any observed real vars in this row then include them in the responsibility
@@ -103,12 +103,15 @@ class MMM(Model):
                     o_r_mask = np.logical_and(~mask_row, self.real_columns)
                     rs[n, :] *= np.array([stats.multivariate_normal.pdf(x_row[o_r_locs], mean=self.μs[k, o_r_locs], cov=self.Σs[k][np.ix_(o_r_mask, o_r_mask)], allow_singular=True)
                         for k in range(self.num_components)])
+
                 # if there are any observed discrete vars then include them
                 if o_d_locs.size:
                     rs[n, :] *= np.array([np.prod([stats.multinomial.pmf(self.one_hot_lookups[d][x_row[d]], 1, self.ps[k, d]) for d in o_d_locs])
                         for k in range(self.num_components)])
             else:
                 rs[n, :] = np.mean(self.rs, axis=0)
+
+        self.rs = rs/np.sum(rs, axis=1, keepdims=True)
 
     # M-step
     def _update_params(self):
@@ -143,7 +146,8 @@ class MMM(Model):
             C = np.zeros(shape=(self.num_features, self.num_features))
             for n in range(self.N):
                 mask_row = self.X[n, :].mask
-                C[np.ix_(mask_row, mask_row)] += self.Σs[k][np.ix_(mask_row, mask_row)] # TODO: make this work with full cov matrices
+                C[np.ix_(mask_row, mask_row)] += p[n]*self.Σs[k][np.ix_(mask_row, mask_row)]/np.sum(p) # TODO: make this work with full cov matrices
+            
             diff = (X - self.μs[k]).T*np.sqrt(p)
             self.Σs[k] = np.diag(np.sum(diff*diff, axis=1)/np.sum(p)) # Note this will need to change to an outer product for full cov something like np.einsum('ij,ik->ijk', foo, foo)
 
