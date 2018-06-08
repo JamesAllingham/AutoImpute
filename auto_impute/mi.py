6efbbf2795affc9f658d3bac4c576d839124c521
James Allingham <james.allingham@gmail.com>
2018-06-08 12:47:56 +0100

    Added a function for testing LL

--- auto_impute/mi.py
+++ auto_impute/mi.py
@@ -15,22 +15,34 @@ class MeanImpute(Model):
     def __init__(self, data, verbose=None):
         Model.__init__(self, data, verbose=verbose)
 
-        self.expected_X = self.X.data
-        μ = ma.mean(self.X, axis=0)
+        self.expected_X = self.X.data.copy()
+        self.μ = ma.mean(self.X, axis=0)
         
         # if there are no observations in any column of X then use 0.0
-        μ[np.isnan(μ)] = 0
+        self.μ[np.isnan(self.μ)] = 0
 
         # replace all missing values with the mean of the collumn
-        self.expected_X[self.X.mask] = μ[np.where(self.X.mask)[1]]
+        self.expected_X[self.X.mask] = self.μ[np.where(self.X.mask)[1]]
 
         # determine the lls for all of the values
         for n in range(self.N):
             for d in range(self.D):
-                self.lls[n, d] = np.log(stats.norm.pdf(self.expected_X[n, d], loc=μ[d], scale=1e-1)) # adding a little leeway here
+                self.lls[n, d] = np.log(stats.norm.pdf(self.expected_X[n, d], loc=self.μ[d], scale=1e-1)) # adding a little leeway here
 
     def _sample(self, num_samples):
         if self.verbose: 
             print_err("Cannot sample from a mean imputation. Returning the means.")
 
         return np.stack([self.expected_X]*num_samples, axis=0)
+
+    def test_ll(self, test_data):
+        N, D = test_data.shape
+        if not D == self.D: 
+            print_err("Dimmensionality of test data (%s) not equal to dimmensionality of training data (%s)." % (D, self.D))
+
+        lls = np.zeros_like(self.lls)
+        for n in range(self.N):
+            for d in range(self.D):
+                lls[n, d] = np.log(stats.norm.pdf(test_data[n, d], loc=self.μ[d], scale=1e-1))
+
+        return lls
